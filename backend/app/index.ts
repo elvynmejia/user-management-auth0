da0009802141e0e39b8683dbeab79a3c241900dc
Elvyn Mejia <elvynmejia@gmail.com>
2023-01-31 12:53:48 -0800

    feat: adds middleware to check auth

--- backend/app/index.ts
+++ backend/app/index.ts
@@ -1,23 +1,22 @@
 import dotenv from 'dotenv';
-
 dotenv.config();
-
 import express, { Express, Router, Request, Response } from 'express';
+import { auth, requiredScopes } from 'express-oauth2-jwt-bearer';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app: Express = express();
 const router: Router = Router();
 
+const checkJwt = auth();
+
+app.use(cors());
 app.use(express.json());
 app.use(morgan('common'));
 app.use(express.urlencoded({ extended: false }));
 
-app.post('/api/v1/auth0test', (req: Request, res: Response) => {
-  res.send({ user: {
-    userId: 1,
-    email: '8f-example.com',
-    token: 'some-random-token-generated-upon-signup'
-  }});
+app.post('/api/v1/auth0test', checkJwt, (req: Request, res: Response) => {
+  res.status(200).json({ endpoint: '/api/v1/auth0test' });
 });
 
 export default app;
